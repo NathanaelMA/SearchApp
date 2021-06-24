1b3eb66e3544901ba63209ab560fdc2a65c83378
vy-shoe <vadimulam6@gatech.edu>
2021-06-24 19:29:05 -0400

    Add radio button from materialUI and place in another div component

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -3,6 +3,6 @@ import "./header.css"
 export default function Header(){
     return <div>
             <img className="header-image" src="https://i.imgur.com/kvT8u4n.jpg?2" />
-            <h1 className="title">Welcome to the user search platform</h1>  
+            <h1 className="title">WELCOME TO USER SEARCH PORTAL</h1>  
         </div>
 }
--- src/component/searchContainer/Search.js
+++ src/component/searchContainer/Search.js
@@ -3,6 +3,12 @@ import "./Search.css"
 import TextField from '@material-ui/core/TextField';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+
 
 class Search extends Component {
 
@@ -10,8 +16,18 @@ class Search extends Component {
 
     render() {
         return (
-            <div>
-                <div className="searchSection">
+            <div className="searchItems">
+                <div>
+                <FormControl component="fieldset">
+                    <FormLabel component="legend">Search By</FormLabel>
+                    <RadioGroup> 
+                        {/* value={value} onChange={handleChange} */}
+                        <FormControlLabel value="name" control={<Radio />} label="Name" />
+                        <FormControlLabel value="city" control={<Radio />} label="City" />
+                    </RadioGroup>
+                </FormControl>
+                </div>
+                <div className="searchBar">
                     <form className="search" noValidate autoComplete="off">
                         <TextField id="search-field" label="Search User/City" placeholder="Jane Doe"/>
                     </form>
