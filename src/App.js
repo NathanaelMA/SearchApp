249553f7652b2e78b7850bf9e4ec810ca49f4d86
vy-shoe <vadimulam6@gatech.edu>
2021-06-25 23:34:34 -0400

    Add a data table as example of where to put our data in DataTable.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import Header from "./component/header"
-import './App.css';
+import Header from "./component/header";
+import "./App.css";
 import Search from "./component/searchContainer/Search.js";
+import DataTable from "./component/dataTable/DataTable.js";
 
 function App() {
-  return (
-    <div className="App">
-    <Header/>
-    <Search />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Search />
+            <DataTable />
+        </div>
+    );
 }
 
 export default App;
