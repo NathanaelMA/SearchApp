b653cb20110afefd6bbe63768e0c0ef0e4715150
mna1021 <76842973+mna1021@users.noreply.github.com>
2021-08-13 18:17:17 -0400

    fix for hosting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Header from "./component/header";
 import "./App.css";
-import Search from "./component/searchContainer/Search.js";
 import DataTable from "./component/dataTable/DataTable";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Search /> */}
       <DataTable />
     </div>
   );
